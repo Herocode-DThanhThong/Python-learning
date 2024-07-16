90268cf23233a7e49001dce25f161f71a56adacd
DuongThanhThong <thongduong61134@gmail.com>
2024-07-16 23:37:16 +0700

    first commit

---
+++ welcome.py
@@ -0,0 +1,12 @@
+greeting = 'Hello world!'
+print(greeting)
+
+line01 = '********************'
+line02 = '**'
+line03 = 'welcome'
+line01 = "123"
+
+print('')
+print(line01)
+print(line02)
+print(line03)
